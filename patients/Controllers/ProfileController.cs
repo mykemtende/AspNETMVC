b74bc8ab2618e130fe4786ec5af4b6788f38aa61
Mtende michael <mtendemichael@gmail.com>
2017-08-25 10:55:34 +0300

    Add files via upload

---
+++ patients/Controllers/ProfileController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Patients.Models;
+
+namespace Patients.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private ProfileDBContext db = new ProfileDBContext();
+
+        // GET: /Profile/
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Patients.ToListAsync());
+        }
+
+        // GET: /Profile/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = await db.Patients.FindAsync(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // GET: /Profile/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Profile/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="ID,UserID,FirstName,LastName,Email,BirthDay,Address,Country,City,Place")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patients.Add(profile);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(profile);
+        }
+
+        // GET: /Profile/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = await db.Patients.FindAsync(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: /Profile/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="ID,UserID,FirstName,LastName,Email,BirthDay,Address,Country,City,Place")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(profile).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(profile);
+        }
+
+        // GET: /Profile/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = await db.Patients.FindAsync(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: /Profile/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Profile profile = await db.Patients.FindAsync(id);
+            db.Patients.Remove(profile);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
