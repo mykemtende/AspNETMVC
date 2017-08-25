4f8ecaa98cc486fc4c57994c30b21a0ce37ad69f
Mtende michael <mtendemichael@gmail.com>
2017-08-25 10:52:31 +0300

    Update AddPatient.cs

--- patients/AddPatient.cs
+++ patients/AddPatient.cs
@@ -1,1 +1,32 @@
 
+using System;
+using System.Data.Entity;//takes care of  
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Patients.Models
+{
+    public class AddPatient
+    {
+        public int Patientcode {get;set;}
+        public string name{get;set;}
+        public string FHname { get; set; }
+        public string Complaint { get; set; }
+        public string Sex { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Place { get; set; }
+        public string Inpatient { get; set; }
+        public string Doctorcode { get; set; }
+        public string Testcode { get; set; }
+        public string Roomcode { get; set; }
+        public DateTime AdmittDate{get;set;}
+    }
+    public class AddPatientDBContext:DbContext
+{
+    public DbSet<AddPatient>Patients{get;set;}
+}
+  
+}
