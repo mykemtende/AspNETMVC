2e1a5d70dbe1439eb9e65421862054341a15e227
Mtende michael <mtendemichael@gmail.com>
2017-08-25 10:57:50 +0300

    Update Profile.cs

--- patients/Models/Profile.cs
+++ patients/Models/Profile.cs
@@ -1,1 +1,26 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace Patients.Models
+{
+    public class Profile
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Place { get; set; }
+    }
+    public class ProfileDBContext : DbContext
+    {
+        public DbSet<Profile> Patients { get; set; }
+    }
+}
